3b2c1c02e83cf39d368030eae384ffedd534a2a9
Guest <kid666@88.com>
2025-11-03 17:11:19 +0800

    修复bug

--- bin/autorun.php
+++ bin/autorun.php
@@ -1,7 +1,4 @@
 <?php
-\pms\hook\LifecycleHook::mount(LIFECYCLE_BOOT,function () {
-    $dbConfig = config('redis');
-    if ($dbConfig !== null) {
-        \pms\facade\Cache::init(\pms\facade\Path::getRuntime('cache'));
-    }
+\pms\hook\LifecycleHook::mount(LIFECYCLE_BOOT, function () {
+    \pms\facade\Cache::init(\pms\facade\Path::getRuntime('cache'));
 });
